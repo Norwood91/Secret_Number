0a9fc7d9a1d0cbeee3b71c32b4c46e3f523a9a50
Robert Norwood <you@example.com>
2023-02-11 10:04:28 -0800

    Created the initial main function to start the game. Created the initial print statements to introduce the player to the game rules. Imported the random module to use later on in the game.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('this is the start of the game!')
+import random
+
+NUM_OF_DIGITS = 3
+MAX_NUM_OF_GUESSES = 10
+
+def main():
+    print(f"I'm thinking of a {NUM_OF_DIGITS} digit number with no repeated numbers.")
+    print("Try to guess what it is!\nHere are some clues. When I say:")
+    print("'Pico'   that means: One digit is correct but in the wrong position.")
+    print("'Fermi'  that means: One digit is correct and in the right position.")
+    print("'Bagels' that means: No digit is correct.")
+    print("For example: if the secret number is 248 and your guess was 843, the clues would be Fermi Pico.")
+
+
+
+if __name__ == "__main__":
+    main()
